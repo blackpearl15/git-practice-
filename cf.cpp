a74de392587f14b68aa187099171dfc38a1bfa46
Yash <yashanand2477@gmail.com>
2021-08-26 19:01:46 +0530

    Changed1

--- cf.cpp
+++ cf.cpp
@@ -18,12 +18,7 @@ using namespace std;
 
 
 void io(){
-    #ifndef ONLINE_JUDGE
-    // for getting input from input.txt
-    freopen("input.txt", "r", stdin);
-    // for writing output to output.txt
-    freopen("output.txt", "w", stdout);
-    #endif
+
 
     /* stuff you should look for
     * read the questions twice (might have missed something imp.)
